bb03d92f6a72382bc0cca3437430bebe104000ac
Paul Oprea <omd.paul@gmail.com>
2018-09-20 15:54:42 +0300

    xxx

--- Furgoneta/administrator/AllDocumentsTableViewController.swift
+++ Furgoneta/administrator/AllDocumentsTableViewController.swift
@@ -149,14 +149,20 @@ class AllDocumentsTableViewController: UITableViewController {
                 }
         }
     }
-    /*
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if  segue.identifier == "showDocuments",
+            let destination = segue.destination as? DocumentViewController,
+            let documentIndex = tableView.indexPathForSelectedRow?.row
+        {
+            destination.documentTypeId = documents[documentIndex].typeDocId
+        }
     }
-    */
+  
 
 }
